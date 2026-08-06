1246aabfcd4a1b6c07e5ca59e9c3d11542cd6cf6
ZᗩYᑎ CᗩPITAᒪ <zycpita@192.168.1.4>
2026-08-06 14:09:07 +0200

    Complete Unit 5 Contact Book project

--- Unit 5/task3.py
+++ Unit 5/task3.py
@@ -16,11 +16,67 @@ def add_contact():
     }
 
 #Add it to the list
+
     contacts.append(contact)
 
     print ("contact added successfully")
-#Call the function
-add_contact()
 
-#Display the contact list
-print(contacts)
+def view_all():
+    print("===================")
+    print("   ALL CONTACTS")
+    print("===================")
+
+    for contact in contacts:
+        print(f"Name : {contact['name']}")
+        print(f"Phone: {contact['phone']}")
+        print(f"Email: {contact['email']}")
+        print("-------------------")
+
+def search_contact():
+    search_name = input("Enter contact name: ")
+
+    for contact in contacts:
+        if contact["name"] == search_name:
+            print(f"Name : {contact['name']}")
+            print(f"Phone : {contact['phone']}")
+            print(f"Email : {contact['email']}")
+
+#Deleting contacts from the contact book...
+
+def delete_contact():
+    search_name = input ("Enter the name you want to delete: ")
+
+    for contact in contacts: 
+        if contact["name"] == search_name :
+            contacts.remove(contact)
+            print("Contact removed succesfully!")
+            break
+
+#Adding while loop to the contact book. NB: While loop - repeats something while it is true
+
+while True: 
+    print("=========================")
+    print("     Contact Book")
+    print("=========================")
+    print("1. Add Contact")
+    print("2. Search contact")
+    print("3. Delete contacts")
+    print("4. view all: ")
+    print("5. Exit: ")
+    
+    choice = input("choose an option: ")
+    if choice == "1" : 
+        add_contact()
+    elif choice == "2" : 
+        search_contact()
+    elif choice == "3" :
+        delete_contact()
+    elif choice == "4" : 
+        view_all()
+    elif choice == "5":
+        print("Thank you for using contact Book")
+        break
+    
+
+    
+        
